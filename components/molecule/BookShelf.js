3e060dd239044f403f5590f58b96f72f919c324b
conan-lab <97595352+conan-lab@users.noreply.github.com>
2022-01-16 08:05:27 +0200

    a

--- components/molecule/BookShelf.js
+++ components/molecule/BookShelf.js
@@ -1,3 +1,4 @@
+import Shelf from "@/public/img/Shelf";
 import { books } from "@/public/js/data";
 import { styles } from "@/public/js/styles";
 import Link from "next/link";
@@ -5,6 +6,7 @@ import { useState } from "react";
 
 export default function BookShelf() {
   const [search, setSearch] = useState("");
+  var shelfs = new Array(7).fill(0);
   return (
     <>
       <div className="shelfContainer">
@@ -15,6 +17,13 @@ export default function BookShelf() {
             placeholder="ابحث عن الكتاب"
             onChange={(e) => setSearch(e.target.value)}
           />
+        </div>
+        <div className="library">
+          {shelfs.map((shelf, i) => (
+            <div key={i} className="shelf">
+              <Shelf />
+            </div>
+          ))}
         </div>
         <div className="bookShelf">
           {books
@@ -34,6 +43,21 @@ export default function BookShelf() {
       <style jsx>{`
         .shelfContainer {
           padding: 2rem;
+          max-width: 50rem;
+          margin: auto;
+        }
+        .library {
+          width: 100%;
+          position: absolute;
+          right: 0;
+          z-index: -2;
+          padding-top: 2rem;
+          max-width: 50rem;
+          margin: auto;
+        }
+        .shelf {
+          width: 100%;
+          padding: 0rem 1rem;
         }
         .inputContainer {
           width: 100%;
@@ -50,10 +74,11 @@ export default function BookShelf() {
           ${styles.flex};
           justify-content: space-evenly;
           flex-wrap: wrap;
-          gap: 2rem 1rem;
+          gap: 5rem 2rem;
           padding: 2rem 0rem;
         }
         .cardContainer {
+          background: white;
           flex: 0 1;
           border: 1px solid gray;
           border-bottom: 3px solid ${styles.primaryColor};
@@ -70,6 +95,10 @@ export default function BookShelf() {
           border-radius: 0 0.6rem 0.6rem 0;
           font-size: 1.2rem;
           text-align: center;
+          cursor: pointer;
+        }
+        .cardContainer:hover {
+          ${styles.boxshadow}
         }
         .date {
           font-size: 0.8rem;
---
+++ public/img/Shelf.js
@@ -0,0 +1,35 @@
+export default function () {
+  return (
+    <svg
+      width="100%"
+      height="502"
+      viewBox="0 0 4573 502"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 502L319.813 113.5H4180.18L4573 502H0Z"
+        fill="url(#paint0_linear_29_20)"
+      />
+      <rect x="77.5308" y="69.5" width="4342.99" height="90" fill="#F2F2F2" />
+      <path
+        d="M77.5308 66L406.389 1L4115.57 2.5L4423.11 66H77.5308Z"
+        fill="#D3D3D3"
+        stroke="black"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_29_20"
+          x1="2286.5"
+          y1="113.5"
+          x2="2286.5"
+          y2="502"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#848484" />
+          <stop offset="1" stop-color="white" stop-opacity="0" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
