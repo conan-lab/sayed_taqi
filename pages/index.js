da2b3ac6bda26ba89eb3b1baef7ab62ed4c87568
conan-lab <97595352+conan-lab@users.noreply.github.com>
2022-01-21 19:40:52 +0200

    aa

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Header from "@/components/molecule/Header";
 import BookShelf from "@/components/molecule/BookShelf";
 import Flower from "@/public/img/Flower";
+import Head from "next/head";
 
 export default function IndexPage() {
   return (
     <>
+      <Head>
+        <title>مكتبة السيد تقي الموسوي</title>{" "}
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div>
         <Header />
         <BookShelf />
